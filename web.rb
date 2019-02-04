bd00e6d065d7e2086c6034b6ef90143b811d86cc
raa0121 <raa0121@gmail.com>
2019-02-04 19:02:10 +0900

    add some func

--- web.rb
+++ web.rb
@@ -1,6 +1,7 @@
 require 'bundler'
 Bundler.require
 require 'discordrb/webhooks'
+require 'open-uri'
 
 
 class TrelloDiscord < Sinatra::Base
@@ -11,41 +12,72 @@ class TrelloDiscord < Sinatra::Base
   end
 
   def init_discord
-    p ENV
     @discord = Discordrb::Webhooks::Client.new(url: ENV['DISCORD_WEBHOOK_URL'])
   end
 
-  get '/trello-discord' do
+  get '/' do
     'hi'
   end
   
-  post '/trello-discord' do
+  post '/' do
     json = JSON.parse request.body.read, symbolize_names: true
-    puts json
+    puts JSON.pretty_generate(json)
     action_type = json[:action][:type]
-    list_name = json[:action][:data][:list][:name]
     card_name = json[:action][:data][:card][:name]
     user_name = json[:action][:memberCreator][:username]
     short_link = 'https://trello.com/c/' + json[:action][:data][:card][:shortLink]
     case action_type
     when 'commentCard'
       title = 'コメント'
+      list_name = json[:action][:data][:list][:name]
       text = json[:action][:data][:text]
-      message = "#{user_name}さんが#{list_name}の#{card_name}にコメントしました\n#{short_link}"
+      message = "#{user_name}さんが「#{list_name}」の「#{card_name}」にコメントしました\n#{short_link}"
     when 'createCard'
       title = 'カード作成'
+      list_name = json[:action][:data][:list][:name]
       text = card_name
-      message = "#{user_name}さんが#{list_name}に#{card_name}を作成しました\n#{short_link}"
+      message = "#{user_name}さんが「#{list_name}」に「#{card_name}」を作成しました\n#{short_link}"
+    when 'updateCheckItemStateOnCard'
+      if 'complete' == json[:action][:data][:checkItem][:state]
+        title = 'チェックボックスON'
+      else
+        title = 'チェックボックスOFF'
+      end
+      text = card_name
+      checklist = json[:action][:data][:checklist][:name]
+      checkitem = json[:action][:data][:checkItem][:name]
+      message = "#{user_name}さんが「#{card_name}」のチェックリスト「#{checklist}」の「#{checkitem}」に#{title}しました\n#{short_link}"
+    when 'addAttachmentToCard'
+      title = 'ファイル添付'
+      list_name = json[:action][:data][:list][:name]
+      text = json[:action][:data][:attachment][:name]
+      message = "#{user_name}さんが「#{list_name}」の「#{card_name}」に「#{text}」を添付しました\n#{short_link}"
     else
       return
     end
-    @discord.execute do |builder|
-      builder.content = message
-      builder.add_embed do |embed|
-        embed.title = title
-        embed.description = text
-        embed.timestamp = Time.now
+    embeds = [Discordrb::Webhooks::Embed.new(
+      timestamp: Time.now,
+      title: title,
+      description: text
+    )]
+    file = nil
+    if action_type == 'addAttachmentToCard'
+      open(json[:action][:data][:attachment][:url]) do |file|
+        open(text, 'w+b') do |out|
+          out.write(file.read)
+        end
       end
+      file = open(text)
+    end
+    builder = Discordrb::Webhooks::Builder.new(content: message, embeds: embeds, file: file)
+    begin
+      @discord.execute(builder)
+    rescue RestClient::ExceptionWithResponse => e
+      p e.http_body
+    end
+    if action_type == 'addAttachmentToCard'
+      file.close
+      File.unlink text
     end
   end
 end
